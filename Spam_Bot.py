16e24b3475322fd8a7ff076e2cb28777754edbe0
Kaor00 <129654268+Kaor00@users.noreply.github.com>
2023-04-02 13:37:13 +0300

    Update Spam_Bot.py

--- Spam_Bot.py
+++ Spam_Bot.py
@@ -4,7 +4,7 @@ import time
 
 time.sleep(5)
 
-with open("/home/kaor/PycharmProjects/Qwark/Download_by_PY/Examples/spaming_text.txt") as file:
+with open("/полный путьt.txt") as file:
     for word in file:
         pyautogui.typewrite(word)
         pyautogui.press("enter")
