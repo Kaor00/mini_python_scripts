e3128303d47152c923c98fee58c39a7cd6d0e4fd
Kaor00 <129654268+Kaor00@users.noreply.github.com>
2023-04-02 13:14:28 +0300

    Update Convert_Txt_to_Audio.py

--- Convert_Txt_to_Audio.py
+++ Convert_Txt_to_Audio.py
@@ -3,7 +3,7 @@ from gtts import gTTS
 audio = 'audio.mp3'
 language = 'ru'
 
-sp = gTTS(text="Ума, меня попросили передать признание в любви."
+sp = gTTS(text="Привет, меня попросили передать признание в любви."
                " Я хороший робо голос и с радостью передаю сообщение!",
           lang=language, slow=False)
 
