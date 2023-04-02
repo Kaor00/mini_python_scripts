ee16ef1b6577963bd2cd5f2f77d6943929cb26bf
Kaor00 <129654268+Kaor00@users.noreply.github.com>
2023-04-02 13:15:43 +0300

    Update Cut_movie.py

--- Cut_movie.py
+++ Cut_movie.py
@@ -1,11 +1,11 @@
 from moviepy.editor import *
 
 
-clip = VideoFileClip('/home/kaor/PycharmProjects/Qwark/Download_by_PY/Examples/AAA.mp4')
+clip = VideoFileClip('')
 
 clip = clip.subclip(0, 30)
 
 # clip.ipython_display(width=360)
 # Команда проигрывания через питона не работает
 
-clip.write_videofile('/home/kaor/PycharmProjects/Qwark/Download_by_PY/Examples/clip.mp4')
+clip.write_videofile('')
